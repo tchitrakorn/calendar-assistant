4df3c82ee0ec2669876b9f6ce0399859fe4727cd
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2023-10-17 20:39:41 -0400

    Added simple validation, error handling, and input sanitization  using express-validator

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, matchedData, validationResult } = require('express-validator');
 const path = require('path');
 const bodyParser = require('body-parser');
 const itemController = require('./controllers/itemControllers');
@@ -11,45 +12,69 @@ app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Answers questions related to the user's current calendar
-app.get('/track', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.track(prompt);
-  res.send(response);
+app.get('/track', body('prompt').notEmpty().escape(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.track(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 // Creates, modifies, or deletes an event based on user requirements
-app.post('/manage', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.manage(prompt);
-  res.send(response);
+app.post('/manage', body('prompt').notEmpty().escape(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.manage(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 // Retrieves online information and personalized recommendations
-app.get('/explore', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.explore(prompt);
-  res.send(response);
+app.get('/explore', body('prompt').notEmpty().escape(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.explore(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 // Retrieves non-sensitive information from users’ conversation log for Google Analytics
-app.get('/analytics', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.getAnalytics(prompt);
-  res.send(response);
+app.get('/analytics', body('prompt').notEmpty().escape(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.getAnalytics(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 // Retrieves user information 
-app.get('/data', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.getUserData(prompt);
-  res.send(response);
+app.get('/data', body('prompt').notEmpty().escape(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.getUserData(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 // Stores/manages user information 
-app.post('/data', (req, res) => {
-  const prompt = req.body.prompt;
-  const response = itemController.manageUserData(prompt);
-  res.send(response);
+app.post('/data', body('prompt').notEmpty(), (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const data = matchedData(req);
+    const response = itemController.manageUserData(data.prompt);
+    return res.send(response);
+  }
+  res.send({ errors: errors.array() });
 });
 
 app.listen(port, () => {
