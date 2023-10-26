40fe6112313f2fbcee11d385c9db95b7248018dd
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-10-25 21:58:42 -0400

    test auth, enable /track endpoint (#8)
    * test auth, enable /track endpoint
    
    * resolve branch conflict

--- python_scripts/get_events.py
+++ python_scripts/get_events.py
@@ -1,23 +1,40 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import sys
+import textwrap
 
-from python_scripts.timezone_utils import convertTime, currentTime, userTimeZone
+from timezone_utils import convertTime, currentTime, userTimeZone
 
+from llama_index.tools import FunctionTool
+from llama_index.llms import OpenAI
+from llama_index.agent import ReActAgent, OpenAIAgent
 
-with open('./python_scripts/credentials.json', 'r') as token_file:
-    creds = Credentials.from_authorized_user_file('./python_scripts/credentials.json')
-  
+client_id = sys.argv[1]
+client_secret = sys.argv[2]
+access_token = sys.argv[3]
+refresh_token = sys.argv[4]
+openai_key = sys.argv[5]
+
+credentials = Credentials(
+            token=access_token,
+            refresh_token=refresh_token,
+            token_uri="http://localhost:8080/oauth2callback", 
+            client_id=client_id,
+            client_secret=client_secret,
+        )
+
 try:
-    service = build('calendar', 'v3', credentials=creds)
+    service = build('calendar', "v3", credentials=credentials)
+    # print("Successfully connected to Google Calendar API")
 except HttpError as error:
     print("An error occurred: ", error)
 
-def upcomingEvents(service, CLIENT_CITY)-> str:
+def upcomingEvents()-> str:
     """Print the start and name of upcoming events (up to 10) on the user's calendar"""
 
     # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    localTimeZone = userTimeZone(CLIENT_CITY)
+    localTimeZone = userTimeZone("New York")
     now = currentTime(localTimeZone)
     output = 'Getting the upcoming 10 events\n'
     events_result = service.events().list(calendarId='primary', timeMin=now,
@@ -34,7 +51,23 @@ def upcomingEvents(service, CLIENT_CITY)-> str:
               + f"   - Local Time Zone: {str(localTimeZone)}" + " " + str(convertTime(start, localTimeZone)) + "\n"
     return output
 
+# Initialize agent with tools available to use and a specific LLM
+upcomingEvents_tool = FunctionTool.from_defaults(fn=upcomingEvents)
+# createEvent_tool = FunctionTool.from_defaults(fn=createEvent)
 
+system_prompt = "When reporting event times, use the local time zone corresponding to the user's city."
+llm = OpenAI(model = "gpt-3.5-turbo", temperature = 0.2, \
+             api_key = openai_key, system_prompt = system_prompt)
+
+agent = ReActAgent.from_tools([upcomingEvents_tool], llm=llm, verbose=True)
+
+
+def wrap_text(string, width=60):
+  wrapped_text = textwrap.wrap(string, width=width)
+  formatted_text = '\n'.join(wrapped_text)
+  return formatted_text
 # upcomingEvents(service, "Guangzhou")
-events = upcomingEvents(service, "Guangzhou")
-print(events)
+# events = upcomingEvents(service, "New York")
+# print(events)
+
+ans = agent.chat("what is my upcoming events")
--- src/models/calendarModel.js
+++ src/models/calendarModel.js
@@ -1,39 +1,59 @@
 // Models for calendar-related tasks
 const { PythonShell } = require('python-shell')
+const queries = require('../database/queries')
 
-const readCalendar = (prompt) => {
-  // The code commented out uses pythonShell to run python scripts, which handles
-  // the logic of retrieving events from the user's calendar. However, we are actively
-  // developing the authorization process to enable this functionality, so for now we
-  // are just returning dummy data.
+const readCalendar = async (email, prompt) => {
+  try {
+    const userArray = await queries.getUser(email)
+    // console.log('User data retrieved:', user);
 
-  // const options = {
-  //   mode: 'text',
-  //   // pythonPath: '/Users/karenwang/.virtualenvs/calendar-assistant/bin/python',  this will be the python path on the server
-  //   pythonOptions: ['-u'], // get print results in real-time
-  //   scriptPath: 'python_scripts'
-  // }
+    if (userArray.length === 0) {
+      throw new Error('User not found')
+    }
+    const user = userArray[0]
 
-  // try {
-  //   // Wait for the result of the Python script
-  //   const results = await new Promise((resolve, reject) => {
-  //     PythonShell.run('auth_get_events.py', options, function (err, results) {
-  //       if (err) reject(err)
-  //       resolve(results)
-  //     })
-  //   })
+    const { id, email: userEmail, client_id, client_secret, openai_key, access_token, refresh_token, city} = user;
+    // console.log('Access Token:', access_token);  // Log the individual properties
+    // console.log('Client ID:', client_id);
 
-  //   return results
-  return `${prompt} read calendar response`
-  // } catch (error) {
-  //   console.error('An error occurred: ', error)
-  //   // Instead of throwing the error, we return dummy data
-  //   return {
-  //     success: false,
-  //     events: [], // or any other dummy data structure you need
-  //     message: 'Still under testing, this is dummy data.'
-  //   }
-  // };
+    const options = {
+      mode: 'text',
+      // pythonPath: '/Users/karenwang/.virtualenvs/calendar-assistant/bin/python',  this will be the python path on the server
+      pythonOptions: ['-u'], // get print results in real-time
+      scriptPath: 'python_scripts',
+      args: [client_id, client_secret, access_token, refresh_token, openai_key]
+    }
+
+    // Execute the Python script and capture the output
+    const scriptOutput = await new Promise((resolve, reject) => {
+      const output = [];
+
+      const pyshell = new PythonShell('get_events.py', options)
+
+      // Collect script print output
+      pyshell.on('message', function (message) {
+        output.push(message)
+      })
+
+      // end the input stream and allow the process to exit
+      pyshell.end(function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(output)
+        }
+      })
+    })
+
+    // 'scriptOutput' will contain the full stdout text from the Python script.
+    return scriptOutput // return it as the response
+  } catch (error) {
+    console.error('An error occurred:', error)
+    return {
+      success: false,
+      message: 'An error occurred while running the calendar script.'
+    }
+  }
 }
 
 const writeCalendar = (prompt) => {
