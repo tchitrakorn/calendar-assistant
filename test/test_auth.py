38fafbe63afb28d4e0cc9373a409f96b2a467fba
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-12-07 10:40:39 -0500

    delete python scripts

--- test/test_auth.py
+++
@@ -1,5 +0,0 @@
-# Since auth.py is still a work in progress, we would like to wait for it to be completed before we start testing it
-
-# We have tried multiple ways to test auth.py, but we cannot find a way to bypass the step of inputting an auth code
-# in the terminal, which is not ideal for automated testing
-
